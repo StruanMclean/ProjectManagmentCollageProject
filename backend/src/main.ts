8961c01e5c3d2cd9ad25f412ebe76c2c6883dc08
Struan Mclean <you@example.com>
2025-03-10 13:55:26 +0000

    build

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,10 +12,7 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: [
-      'http://127.0.0.1:3000',
-      'http://localhost:3000'
-    ],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
