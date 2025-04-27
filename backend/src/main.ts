8bcc3f84966e9410bb6ca1113d621edabc13be71
Struan Mclean <struanmclean16@gmail.com>
2025-04-27 17:52:46 +0100

    make cores any for anyone downloading

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,10 +12,7 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000"
-    ],
+    origin: ["*"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
